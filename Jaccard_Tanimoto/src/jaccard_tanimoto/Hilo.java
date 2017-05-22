51869341e9545b9c467ebbc083e48b467a3dc0be
AndresSal <andres.salazar01@epn.edu.ec>
2017-05-22 13:55:50 -0500

    codigo comentado Hilo

--- Jaccard_Tanimoto/src/jaccard_tanimoto/Hilo.java
+++ Jaccard_Tanimoto/src/jaccard_tanimoto/Hilo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jaccard_tanimoto;
 
 
@@ -16,7 +11,7 @@ import java.util.TreeMap;
 
 /**
  *
- * @author joselimaico
+ * @author joselimaico andressalazar
  */
 public class Hilo extends Thread implements Runnable {
     
